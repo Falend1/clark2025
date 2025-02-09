c11e3c4780b09790c69f884cf4c1cb0ea1c78da8
Clark <lendfa3@gmail.com>
2025-02-09 18:36:01 +0800

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,71 @@
+export const siteConfig: SiteConfig = {
+    title: "Welcome To My Blog!",
+    language: "zh",
+    description: "Clark's personal blog. Powered by Astro Blog Theme Blur.",
+    keywords: "Clark, blog, personal blog, Astro, Astro Blog Theme Blur",
+    author: "Clark",
+    avatar: "/JianShi.png",
+    favicon: "/JianShi.png",
+    site: "https://Clark.top",
+
+    page_size: 10,
+}
+
+export const navBarConfig: NavBarConfig = {
+    links: [
+        {
+            name: 'Projects',
+            url: '/projects'
+        },
+        {
+            name: 'Links',
+            url: '/links'
+        },
+        {
+            name: 'About',
+            url: '/about'
+        }
+    ]
+}
+
+export const socialLinks: SocialLink[] = [
+    // https://icon-sets.iconify.design/material-symbols/
+    {
+        label: 'GitHub',
+        icon: 'mdi-github',
+        url: 'https://github.com/Clark6'
+    },
+    {
+        label: 'Email',
+        icon: 'material-symbols:mail-rounded',
+        url: 'mailto:Clark@jaze.top'
+    }
+]
+
+interface SiteConfig {
+    title: string
+    language: string
+    description: string
+    keywords: string
+    author: string
+    avatar: string
+    favicon: string
+    site: string
+
+    page_size: number
+    twikoo_uri?: string     // https://twikoo.js.org/
+}
+
+interface NavBarConfig {
+    links: {
+        name: string
+        url: string
+        target?: string
+    }[]
+}
+
+interface SocialLink {
+    label: string
+    icon: string
+    url: string
+}
---
+++ src/content/posts/My Blog.md
@@ -0,0 +1,10 @@
+---
+title: My Blog
+description: Have A Try Then Try Your Best
+pubDate: 2025-02-08
+---
+# My first time to create a blog
+
+## Can you give me some suggestions about my profile,so that I can decorate it prefectly!
+
+### Thanks!!!
